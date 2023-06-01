609a4e25dbdefa77a0254e499c0c595c8861228a
DhysonRodrigues <131685850+DhysonRodrigues@users.noreply.github.com>
2023-06-01 11:12:46 -0300

    segue solicitaçao

--- src/services/serviceTurma.ts
+++ src/services/serviceTurma.ts
@@ -3,12 +3,50 @@ import { AppDataSource } from "../databases/connections/data-source"
 
 const cursor = AppDataSource.getRepository(Turma)
 
-export class CreateCursoService {}
+// Captura os dados vindos do frontend via requisição
+type newTurmaRequest = {
+    id_turma: string
+    fk_curso: string
+    data_inicio: Date
+    data_fim: Date
+    horas_aula_dia: number
+}
 
+export class CreateTurmaService {
+    // passa os dados da requisição como parametro do método "execute()"
+    async execute({
+        id_turma,
+        fk_curso,
+        data_inicio,
+        data_fim,
+        horas_aula_dia,
+}: newTurmaRequest): Promise<Turma | Error> {
+        // Se já existir um curso com a mesma descrição informada pelo usuário
+        // o sistema retornará uma mensagem de erro
+        if (await cursor.findOne({ where: { id_turma } })) {
+        return new Error("Turma já cadastrado!")
+        }
+    
+// Cria um objeto (APP) para ser salvo como registro (BD)
+    const turma = cursor.create({
+        id_turma,
+        fk_curso,
+        data_inicio,
+        data_fim,
+        horas_aula_dia,
+})
+    // Faz um INSERT lá na tabela "curso"
+    // com os dados informados pelo usuário
+    await cursor.save(turma)
+
+    // Devolve pro frontend o objeto criado da classe "Curso"
+    return turma
+}
+}
 export class ReadAllCursoService {}
 
 export class ReadOneCursoService {}
 
 export class UpdateCursoService {}
 
-export class DeleteCursoService {}
+export class DeleteCursoService {}
--- src/services/serviceUnidade.ts
+++ src/services/serviceUnidade.ts
@@ -1,9 +1,44 @@
-import Curso from "../databases/models/curso"
+import Unidade from "../databases/models/unidade"
 import { AppDataSource } from "../databases/connections/data-source"
 
-const cursor = AppDataSource.getRepository(Curso)
+const cursor = AppDataSource.getRepository(Unidade)
 
-export class CreateCursoService {}
+// Captura os dados vindos do frontend via requisição
+type newUnidadeRequest = {
+    id_unidade : string
+    descricao_unidade: string
+    carga_horaria_unidade: number
+    ordem: number
+    fk_curso:string
+}
+export class CreateUnidadeService {
+    // passa os dados da requisição como parametro do método "execute()"
+async execute({
+    id_unidade, 
+    descricao_unidade,
+    carga_horaria_unidade,
+    ordem,
+    fk_curso,
+}: newUnidadeRequest): Promise<Unidade | Error> {
+     // Se já existir um curso com a mesma descrição informada pelo usuário
+        // o sistema retornará uma mensagem de erro
+    if (await cursor.findOne({ where: { descricao_unidade } })) {
+    return new Error("não pode matricular em mais de uma unidade!")
+}
+ // Faz um INSERT lá na tabela "curso"
+    // com os dados informados pelo usuário
+const unidade = cursor.create({
+    id_unidade, 
+    descricao_unidade,
+    carga_horaria_unidade,
+    ordem,
+    fk_curso,
+})
+await cursor.save(unidade)
+// Devolve pro frontend o objeto criado da classe "Curso"
+return unidade
+}
+}
 
 export class ReadAllCursoService {}
 
