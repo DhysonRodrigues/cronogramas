27ac8550451bc1368ac7b8e3c1a31d44afaddb6b
DhysonRodrigues <131685850+DhysonRodrigues@users.noreply.github.com>
2023-06-01 11:56:15 -0300

    aa
    aa

--- src/services/serviceTurma.ts
+++ src/services/serviceTurma.ts
@@ -1,9 +1,13 @@
 import Turma from "../databases/models/turma"
 import { AppDataSource } from "../databases/connections/data-source"
 
+
+// 1) Estabelece conexão com a tabela alvo no banco de dados através de um cursor
+
 const cursor = AppDataSource.getRepository(Turma)
 
-// Captura os dados vindos do frontend via requisição
+// 2) Recebe dados da Requisição HTTP lá do FRONTEND
+
 type newTurmaRequest = {
     id_turma: string
     fk_curso: string
@@ -12,29 +16,36 @@ type newTurmaRequest = {
     horas_aula_dia: number
 }
 
+type findOneTurmaRequest = {
+    id_turma: string
+}
+
+// 3) Classes CRUD
+
 export class CreateTurmaService {
-    // passa os dados da requisição como parametro do método "execute()"
-    async execute({
-        id_turma,
-        fk_curso,
-        data_inicio,
-        data_fim,
-        horas_aula_dia,
+  // passa os dados da requisição como parametro do método "execute()"
+async execute({
+    id_turma,
+    fk_curso,
+    data_inicio,
+    data_fim,
+    horas_aula_dia,
 }: newTurmaRequest): Promise<Turma | Error> {
-        // Se já existir um curso com a mesma descrição informada pelo usuário
-        // o sistema retornará uma mensagem de erro
-        if (await cursor.findOne({ where: { id_turma } })) {
-        return new Error("Turma já cadastrado!")
-        }
-    
-// Cria um objeto (APP) para ser salvo como registro (BD)
+    // Se já existir um curso com a mesma descrição informada pelo usuário
+    // o sistema retornará uma mensagem de erro
+    if (await cursor.findOne({ where: { id_turma } })) {
+    return new Error(" turma já cadastrada!")
+    }Turma
+
+    // Cria um objeto (APP) para ser salvo como registro (BD)
     const turma = cursor.create({
         id_turma,
         fk_curso,
         data_inicio,
         data_fim,
         horas_aula_dia,
-})
+    })
+
     // Faz um INSERT lá na tabela "curso"
     // com os dados informados pelo usuário
     await cursor.save(turma)
@@ -43,10 +54,45 @@ export class CreateTurmaService {
     return turma
 }
 }
-export class ReadAllCursoService {}
 
-export class ReadOneCursoService {}
+export class ReadAllCursoService {
+async execute() {
+    // Executa a consulta "SELECT * FROM curso" no BD
+    // Armazena todos os registros do Result Set na variável "cursos"
+    // Neste caso, esta variável é uma lista de cursos
+    const turma = await cursor.find()
+    return turma
+}
+}
+
+export class ReadOneCursoService {
+  // Recebe o ID do curso como parâmetro da Requisição do usuário
+async execute({ id_turma }: findOneTurmaRequest) {
+    // Vê se o curso existe na tabela no BD - SELECT * FROM curso WHERE id_curso = ??
+    const turma = await cursor.findOne({ where: { id_turma } })
+    // Se o curso não for encontrado no Result Set retorna um erro para o usuário
+    if (!turma) {
+    return new Error("turma não encontrado!")
+    }
+    // Se o curso for encontrado retorna para o usuário o curso
+    return turma
+}
+}
 
 export class UpdateCursoService {}
 
-export class DeleteCursoService {}
+export class DeleteCursoService {
+  // Recebe o ID do curso como parâmetro da Requisição do usuário
+async execute({ id_turma }: findOneTurmaRequest) {
+    // Vê se o curso existe na tabela no BD - SELECT * FROM curso WHERE id_curso = ??
+    const turma = await cursor.findOne({ where: { id_turma } })
+    // Se o curso não for encontrado no Result Set retorna um erro para o usuário
+    if (!turma) {
+    return new Error("Curso não encontrado!")
+    }
+    // Se o curso for encontrado, deleta do BD - DELETE FROM curso WHERE id_curso = ??
+    await cursor.delete(turma.id_turma)
+    // Retorna para o usuário o curso que foi deletado
+    return turma
+}
+}
--- src/services/serviceUnidade.ts
+++ src/services/serviceUnidade.ts
@@ -1,9 +1,13 @@
 import Unidade from "../databases/models/unidade"
 import { AppDataSource } from "../databases/connections/data-source"
 
+
+// 1) Estabelece conexão com a tabela alvo no banco de dados através de um cursor
+
 const cursor = AppDataSource.getRepository(Unidade)
 
-// Captura os dados vindos do frontend via requisição
+// 2) Recebe dados da Requisição HTTP lá do FRONTEND
+
 type newUnidadeRequest = {
     id_unidade : string
     descricao_unidade: string
@@ -11,8 +15,15 @@ type newUnidadeRequest = {
     ordem: number
     fk_curso:string
 }
+
+type findOneUnidadeRequest = {
+    id_unidade: string
+}
+
+// 3) Classes CRUD
+
 export class CreateUnidadeService {
-    // passa os dados da requisição como parametro do método "execute()"
+  // passa os dados da requisição como parametro do método "execute()"
 async execute({
     id_unidade, 
     descricao_unidade,
@@ -20,30 +31,68 @@ async execute({
     ordem,
     fk_curso,
 }: newUnidadeRequest): Promise<Unidade | Error> {
-     // Se já existir um curso com a mesma descrição informada pelo usuário
-        // o sistema retornará uma mensagem de erro
-    if (await cursor.findOne({ where: { descricao_unidade } })) {
-    return new Error("não pode matricular em mais de uma unidade!")
-}
- // Faz um INSERT lá na tabela "curso"
-    // com os dados informados pelo usuário
-const unidade = cursor.create({
-    id_unidade, 
+    // Se já existir um curso com a mesma descrição informada pelo usuário
+    // o sistema retornará uma mensagem de erro
+    if (await cursor.findOne({ where: { id_unidade } })) {
+    return new Error(" unidade já cadastrada!")
+    }Unidade
+
+    // Cria um objeto (APP) para ser salvo como registro (BD)
+    const unidade = cursor.create({
+        id_unidade, 
     descricao_unidade,
     carga_horaria_unidade,
     ordem,
     fk_curso,
-})
-await cursor.save(unidade)
-// Devolve pro frontend o objeto criado da classe "Curso"
-return unidade
+    })
+
+    // Faz um INSERT lá na tabela "curso"
+    // com os dados informados pelo usuário
+    await cursor.save(unidade)
+
+    // Devolve pro frontend o objeto criado da classe "Curso"
+    return unidade
 }
 }
 
-export class ReadAllCursoService {}
+export class ReadAllCursoService {
+async execute() {
+    // Executa a consulta "SELECT * FROM curso" no BD
+    // Armazena todos os registros do Result Set na variável "cursos"
+    // Neste caso, esta variável é uma lista de cursos
+    const unidade = await cursor.find()
+    return unidade
+}
+}
 
-export class ReadOneCursoService {}
+export class ReadOneCursoService {
+  // Recebe o ID do curso como parâmetro da Requisição do usuário
+async execute({ id_unidade }: findOneUnidadeRequest) {
+    // Vê se o curso existe na tabela no BD - SELECT * FROM curso WHERE id_curso = ??
+    const unidade = await cursor.findOne({ where: { id_unidade } })
+    // Se o curso não for encontrado no Result Set retorna um erro para o usuário
+    if (!unidade) {
+    return new Error("turma não encontrado!")
+    }
+    // Se o curso for encontrado retorna para o usuário o curso
+    return unidade
+}
+}
 
 export class UpdateCursoService {}
 
-export class DeleteCursoService {}
+export class DeleteCursoService {
+  // Recebe o ID do curso como parâmetro da Requisição do usuário
+async execute({ id_unidade }: findOneUnidadeRequest) {
+    // Vê se o curso existe na tabela no BD - SELECT * FROM curso WHERE id_curso = ??
+    const unidade = await cursor.findOne({ where: { id_unidade } })
+    // Se o curso não for encontrado no Result Set retorna um erro para o usuário
+    if (!unidade) {
+    return new Error("Unidade não encontrado!")
+    }
+    // Se o curso for encontrado, deleta do BD - DELETE FROM curso WHERE id_curso = ??
+    await cursor.delete(unidade)
+    // Retorna para o usuário o curso que foi deletado
+    return unidade
+}
+}
